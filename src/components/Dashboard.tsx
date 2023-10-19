168fd5eb2ffeb938273f8eca2c4bc089f5c39765
Michael Frieze <michaelfrieze@icloud.com>
2023-10-18 20:50:57 -0400

    Added all the shadcn-ui components I am going to need

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import UploadButton from './UploadButton';
+
 const Dashboard = () => {
   return (
     <main className="mx-auto max-w-7xl md:p-10">
---
+++ src/components/UploadButton.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { useState } from 'react';
+
+const UploadButton = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <div>
+      <p>test</p>
+    </div>
+  );
+};
+
+export default UploadButton;
